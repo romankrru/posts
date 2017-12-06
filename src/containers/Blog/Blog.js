3168fbe5bae33df4479891dd3746e0b1f101800e
Roman <rm07ru@gmail.com>
2017-12-06 20:19:13 +0300

    Add data render

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,30 +7,38 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                console.log(response);
-            });
-    }
-    
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+          this.setState({
+            posts: response.data,
+          });
+      });
+  }
+  
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post title={post.title} key={post.id} />
+    });
+
+    return (
+      <div>
+          <section className="Posts">
+            {posts}
+          </section>
+          <section>
+            <FullPost />
+          </section>
+          <section>
+            <NewPost />
+          </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
